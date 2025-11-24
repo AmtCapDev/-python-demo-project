ae129fa5077cb0d9fa31dc31b9503c92418482e7
AmtCapDev <AmtCapDev@proton.me>
2025-11-24 12:23:39 -0500

    Tickers & Data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,66 @@
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+import yfinance as yf
 
+# Portfolio tickers
+tickers = [
+    "ADSK",
+    "AEHR",
+    "AGYS",
+    "ALGM",
+    "ALGN",
+    "AMT",
+    "ARES",
+    "AXON",
+    "BMI",
+    "BURL",
+    "CDNS",
+    "CMG",
+    "CVLT",
+    "CWST",
+    "DT",
+    "DXCM",
+    "EL",
+    "ELF",
+    "ENPH",
+    "EXPI",
+    "EXPO",
+    "GSHD",
+    "HEI",
+    "IDXX",
+    "INTU",
+    "ISRG",
+    "JYNT",
+    "LNTH",
+    "LSCC",
+    "MANH",
+    "MKTX",
+    "MPWR",
+    "NOVT",
+    "NOW",
+    "NSSC",
+    "NVDA",
+    "PANW",
+    "PAYC",
+    "PCTY",
+    "PGNY",
+    "SNPS",
+    "SPSC",
+    "SSTI",
+    "STAA",
+    "TSLA",
+    "TTD",
+    "VEEV",
+]
 
-print(greet("AmtCapDev"))
+# Get EOD adjusted close prices
+prices = yf.download(
+    tickers,
+    start="2023-03-21",
+    end=None,  # None = up to today
+    interval="1d",  # Daily (EOD)
+    group_by="ticker",  # Keeps data organized by ticker
+    auto_adjust=True,  # Adjusts for splits & dividends
+    progress=False,
+)
+
+adj_close = prices.xs("Close", level=1, axis=1)
+print(adj_close.head())
